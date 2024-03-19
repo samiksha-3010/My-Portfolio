de703871d6f25ef037ea4f7b9c408fe73d720933
samiksha-3010 <samiksha30102@gmail.com>
2024-03-19 20:47:15 +0530

    done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,38 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h2>PortFolio</h2>
+
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import styled from 'styled-components'
 import './App.css';
+const H1 = styled.h1`
+color: red;
+`
+
+const DefaultButton = styled.button`
+background-color: black;
+border: none;
+padding: 10px;
+color: white;
+`
 
 function App() {
   return (
-    <div className="App">
-      <h2>PortFolio</h2>
+    <div>
+      <H1>Styled Components</H1>
+      <p>My First styled  Project style Cmponent.</p>
+      <DefaultButton>Click ME!</DefaultButton>
 
+      
     </div>
   );
 }
